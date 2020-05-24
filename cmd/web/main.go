971cb9772a5690a3454639faea5c38d161cec137
Dimitrije <dimitrijed93@gmail.com>
2020-05-24 12:29:34 +0200

    change port

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -11,6 +12,6 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), mux)
 	log.Fatal(err)
 }
