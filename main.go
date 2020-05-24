551e0dfe2c62c838eb75637e6d87608adb6a7940
Dimitrije <dimitrijed93@gmail.com>
2020-05-24 07:59:20 +0200

    http test

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("HEROKUUUUUUUUUUUUUUUU"))
+}
 
 func main() {
 
 	fmt.Println("DSDD")
+	http.Handle("/handle", http.HandlerFunc(handlerFunc))
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
